ff8ee560375cbc5823d1ccbc9f224e34195f1ef7
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2024-12-25 20:14:43 +0400

    add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,7 +20,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
+
+  constructor(value) {
+    this.reverse = value;
+  }
   encrypt(string, key) {
+    if (!string || !key) throw new Error('Incorrect arguments!');
     let strToEncrypt = string.toUpperCase();
     let keyStr = '';
     let encryptedStr = '';
@@ -43,11 +48,35 @@ class VigenereCipheringMachine {
         encryptedStr += String.fromCharCode(char);
       }
     }
+    if (this.reverse === false) return encryptedStr.split('').reverse().join("");
     return encryptedStr;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(string, key) {
+    if (!string || !key) throw new Error('Incorrect arguments!');
+    let encryptedString = string.toUpperCase();
+    let keyStr = '';
+    let decryptedStr = '';
+    let j = 0;
+    for (let i = 0; i < encryptedString.length; i++) {
+      if (encryptedString.charCodeAt(i) < 65 || encryptedString.charCodeAt(i) > 90) {
+        keyStr += encryptedString[i];
+      } else {
+        keyStr = `${keyStr}${key[j % key.length].toUpperCase()}`;
+        j++;
+      }
+    }
+    for (let i = 0; i < encryptedString.length; i++) {
+      if (encryptedString.charCodeAt(i) < 65 || encryptedString.charCodeAt(i) > 90) {
+        decryptedStr += encryptedString[i];
+      } else {
+        let offset = keyStr.charCodeAt(i) - 'A'.charCodeAt();
+        let char = encryptedString.charCodeAt(i) - offset;
+        if (char < 65) char = char + 26;
+        decryptedStr += String.fromCharCode(char);
+      }
+    }
+    if (this.reverse === false) return decryptedStr.split('').reverse().join("");
+    return decryptedStr;
   }
 }
 
